c05854d7485e1642dd6247ad9ca669cbb7f9d089
hemmerian@gmail.com <hemmerian@gmail.com>
2023-03-22 09:03:10 -0700

    Added pragma once to grid.h added test code from a raylib example

--- include/gol/grid.h
+++ include/gol/grid.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <raylib.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -18,28 +19,6 @@ void fnForGrid(grid_s g){
     assert(g.offset.y);
 }*/
 
-typedef struct limited_grid_s {
-    grid_s grid; // fully defined grid dimensions
-    size_t row;  // maximum rows in the grid(max y)
-    size_t col;  // maximum columns in the grid(max x)
-} limited_grid_s;
-/*
-const limited_grid_s LG1 = {
-    {10, (Vector2) {50, 0}}, 20, 10
-    };
-
-void fnForLimitedGrid(limited_grid_s lg){
-    for(size_t x = 0; x < lg.row; x++){
-        for(size_t y = 0; y < lg.col; y++){
-            assert(lg.grid);
-            assert(lg.row);
-            assert(lg.col);
-            assert(x);
-            assert(y);
-        }
-    }
-}*/
-
 /**
  * int, Vector2 -> grid_s
  * creates a valid grid, if input is invalid, returns a size of 1 and offset of (0,0) 
@@ -50,16 +29,16 @@ grid_s grid_create(int size, Vector2 offset);
  * grid_s, Vector2 -> Vector2 
  * converts a grid coordinate to a screen coordinate
  */
-Vector2 grid_to_screen(grid_s grid, Vector2 gc) { return (Vector2){0,0};}
+Vector2 grid_to_screen(grid_s grid, Vector2 gc);
 
 /**
  * grid_s, Vector2 -> Vector2 
  * converts a screen coordinate to its grid coordinate floor
  */
-Vector2 screen_to_grid(grid_s grid, Vector2 sc) { return (Vector2){0,0};}
+Vector2 screen_to_grid(grid_s grid, Vector2 sc);
 
 /**
  * grid_s, Vector2 -> Vector2 
  * converts a screen coordinate to its exact grid coordinate
  */
-Vector2 screen_to_grid_exact(grid_s grid, Vector2 sc) { return (Vector2){0,0};}
+Vector2 screen_to_grid_exact(grid_s grid, Vector2 sc);
--- src/grid.c
+++ src/grid.c
@@ -7,3 +7,9 @@ grid_s grid_create(int size, Vector2 offset) {
         return (grid_s){size, offset};
     }
 }
+
+Vector2 grid_to_screen(grid_s grid, Vector2 gc) { return (Vector2){0,0};}
+
+Vector2 screen_to_grid(grid_s grid, Vector2 sc) { return (Vector2){0,0};}
+
+Vector2 screen_to_grid_exact(grid_s grid, Vector2 sc) { return (Vector2){0,0};}
